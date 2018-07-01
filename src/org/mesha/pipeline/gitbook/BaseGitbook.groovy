e0d5b5aef59a7b0fa8b8395071511b75635300d0
Adam Mesha <adam@mesha.org>
2018-07-02 00:13:09 +0300

    Use vars class implementations of the build steps

--- src/org/mesha/pipeline/gitbook/BaseGitbook.groovy
+++ src/org/mesha/pipeline/gitbook/BaseGitbook.groovy
@@ -34,18 +34,10 @@ abstract class BaseGitbook implements Serializable {
     }
 
     void build() {
-        // Build the docker image using a Dockerfile
-        script.docker.build(image)
-        // override the latest tag
-        script.docker.build(imageLatest)
+        script.dockerBuild(image, imageLatest)
     }
 
     void publish() {
-        script.docker.withRegistry("https://registry.hub.docker.com", "dockerHub")	{
-            // version eq latest git tag
-            script.docker.image(image).push()
-            // override the latest tag
-            script.docker.image(imageLatest).push()
-        }
+        script.gitbookPublish(image, imageLatest)
     }
 }
